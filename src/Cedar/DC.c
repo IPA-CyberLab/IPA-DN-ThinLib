5a33d48cd42c05217464d615c1fe817a11311682
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2022-11-07 19:54:15 +0900

    test

--- src/Cedar/DC.c
+++ src/Cedar/DC.c
@@ -2876,12 +2876,14 @@ void DcGetBestHostnameForPcid(char *hostname, UINT hostname_size, char *pcid, bo
 		return;
 	}
 
+	int timeout = 5000;
+
 	DcGenerateHostname(hostname, hostname_size, pcid, ipv6);
 
 	if (ipv6 == false)
 	{
 		// IPv4 の場合
-		if (GetIP4InnerWithNoCache(&ip, hostname, true, 0) == false)
+		if (GetIP4InnerWithNoCache(&ip, hostname, true, timeout) == false)
 		{
 			// IPv4 ホスト名の取得に失敗
 			b = false;
@@ -2889,7 +2891,7 @@ void DcGetBestHostnameForPcid(char *hostname, UINT hostname_size, char *pcid, bo
 		else
 		{
 			IP ip6 = CLEAN;
-			if (GetIP6InnerWithNoCache(&ip6, hostname, true, 0))
+			if (GetIP6InnerWithNoCache(&ip6, hostname, true, timeout))
 			{
 				// IPv6 ホスト名の取得に成功してしまった。これは使用できない。
 				b = false;
@@ -2904,7 +2906,7 @@ void DcGetBestHostnameForPcid(char *hostname, UINT hostname_size, char *pcid, bo
 	else
 	{
 		// IPv6 の場合
-		if (GetIP6InnerWithNoCache(&ip, hostname, true, 0) == false)
+		if (GetIP6InnerWithNoCache(&ip, hostname, true, timeout) == false)
 		{
 			// IPv6 ホスト名の取得に失敗
 			b = false;
@@ -2912,7 +2914,7 @@ void DcGetBestHostnameForPcid(char *hostname, UINT hostname_size, char *pcid, bo
 		else
 		{
 			IP ip4 = CLEAN;
-			if (GetIP4InnerWithNoCache(&ip4, hostname, true, 0))
+			if (GetIP4InnerWithNoCache(&ip4, hostname, true, timeout))
 			{
 				// IPv4 ホスト名の取得に成功してしまった。これは使用できない。
 				b = false;
