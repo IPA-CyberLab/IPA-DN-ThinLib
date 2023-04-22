8730f41af6a2ccfb7cf28530c1cfb71b293f4d66
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2023-04-23 05:40:43 +0900

    SSL Shared CTX expires と StatMan を一時的に無効化 (デバッグのため)

--- src/Mayaqua/Network.c
+++ src/Mayaqua/Network.c
@@ -14432,7 +14432,7 @@ SSL_CTX_SHARED* GetOrCreateSslCtxShared(LIST* o, SSL_CTX_SHARED_SETTINGS* settin
 		{
 			SSL_CTX_SHARED* s = LIST_DATA(o, i);
 
-			if (s->Expires != 0 && now > s->Expires)
+			if (s->Expires != 0 && now > s->Expires && false)
 			{
 				if (delete_list == NULL)
 				{
@@ -14471,6 +14471,8 @@ SSL_CTX_SHARED* GetOrCreateSslCtxShared(LIST* o, SSL_CTX_SHARED_SETTINGS* settin
 		if (ret == NULL)
 		{
 			ret = NewSslCtxSharedInternal(settings);
+
+			WHERE;
 
 			if (ret != NULL)
 			{
